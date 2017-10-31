1f425613a4370d865473f70eb5e897e170c8d92e
itsTrada <luathieng1130@yahoo.com>
2017-10-31 10:24:51 +0700

    Update Quan ly + Rut tien

--- NganHang/RutTien.cs
+++ NganHang/RutTien.cs
@@ -13,11 +13,68 @@ namespace NganHang
 {
     public partial class RutTien : Form
     {
-        SqlConnection ketnoi = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Vipper\Desktop\NganHang\Database\NganHang.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlConnection ketnoi = new SqlConnection(@"Data Source=DESKTOP-FPNJFC5;Initial Catalog=NganHang;Integrated Security=True");
         public static string user4 = "";
         public RutTien()
         {
             InitializeComponent();
+        }
+        int mathe, tien, tienrut, tongtien = 0;
+        string mk;
+        private void RutTien_Load(object sender, EventArgs e)
+        {
+            ketnoi.Open();
+            SqlCommand command = new SqlCommand("select MaThe from KhachHang where TenDangNhap='" + user4 + "'", ketnoi);
+            mathe = (int)command.ExecuteScalar();
+            SqlCommand command5 = new SqlCommand("select Tien from KhachHang where TenDangNhap='" + user4 + "'", ketnoi);
+            tien = (int)command5.ExecuteScalar();
+            SqlCommand command2 = new SqlCommand("select MatKhau from KhachHang where TenDangNhap='" + user4 + "'", ketnoi);
+            mk = (string)command2.ExecuteScalar();
+            MaThe.Text = mathe.ToString();
+            SoDu.Text = tien.ToString();
+        }
+
+        private void btRut_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tienrut = int.Parse(TienRut.Text);
+                tongtien = tien - tienrut;
+                if (tienrut > tien)
+                {
+                    MessageBox.Show(" Tiền Của Bạn Không Đủ ", "Thông Báo ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                    if (tienrut < 0)
+                    MessageBox.Show(" Nhập Sai ", "Thông Báo ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else if (tienrut > 10000000)
+                {
+                    MessageBox.Show(" Một lần bạn chỉ Rút tối đa được 10 triệu ", "Thông Báo ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                    if (matkhauxacnhan.Text == mk)
+                {
+                    string update = "UPDATE KhachHang SET Tien='" + tongtien + "'WHERE MaThe='" + mathe + "'";
+                    SqlCommand command = new SqlCommand(update, ketnoi);
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Rút Tiền Thành Công", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    SqlCommand command5 = new SqlCommand("select Tien from KhachHang where TenDangNhap='" + user4 + "'", ketnoi);
+                    tien = (int)command5.ExecuteScalar();
+                    SoDu.Text = tien.ToString();
+                }
+                else
+                    MessageBox.Show(" Rút Tiền Không Thành Công ", "Mật Khẩu Sai ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show(" Bạn Nhập Sai ", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                TienRut.Focus();
+            }
+        }
+
+        private void btThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
         protected override bool ProcessDialogKey(Keys keyData)
         {
